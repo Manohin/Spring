925b242d528e8ba5e457eb49170755311f71720e
Manohin <111537419+Manohin@users.noreply.github.com>
2023-03-16 23:00:18 +0300

    Prepare Storyboard and create Model

---
+++ Spring/Model/Animation.swift
@@ -0,0 +1,14 @@
+//
+//  Animation.swift
+//  Spring
+//
+//  Created by Alexey Manokhin on 16.03.2023.
+//
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
