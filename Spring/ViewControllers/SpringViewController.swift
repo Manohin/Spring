11c54f00f72bb544200c49b2c4d7e019a1047292
Manohin <111537419+Manohin@users.noreply.github.com>
2023-03-17 15:42:41 +0300

    Update

--- Spring/ViewControllers/SpringViewController.swift
+++ Spring/ViewControllers/SpringViewController.swift
@@ -20,7 +20,7 @@ final class SpringViewController: UIViewController {
     @IBOutlet var delayLabel: UILabel!
     @IBOutlet var launchButton: UIButton!
     
-    private let animate = Animation.getAnimation
+    private var animate = Animation.getAnimation
     
     
     override func viewDidLoad() {
@@ -30,6 +30,17 @@ final class SpringViewController: UIViewController {
     }
     
     @IBAction func launchButton(_ sender: UIButton) {
+        
+        actionView.animation += animate.preset.rawValue
+        presetAnimationLabel.text = "Preset: " + animate.preset.rawValue
+        curveLabel.text = "Curve: " + animate.curve.rawValue
+        forceLabel.text = "Force: " + String(animate.force)
+        durationLabel.text = "Duration: " + String(animate.duration)
+        delayLabel.text = "Delay: " + String(animate.delay)
+        
+        animate = Animation.getAnimation
+        sender.setTitle("Запустить  \(animate.preset.rawValue)", for: .normal)
+        
     }
     
 }
